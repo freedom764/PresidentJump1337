6041fba145dfffc66ac090b8fb31da9575c7386d
freedom764 <gkfigfo@gmail.com>
2020-05-03 15:05:38 +0600

    движение

---
+++ Mr.President/Assets/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject DoodlerPrefab;
+    public GameObject Doodler;
+    public Transform SpawnPosition;
+    
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Doodler = Instantiate(DoodlerPrefab, SpawnPosition.position, SpawnPosition.rotation) as GameObject;
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementFunction();
+
+    
+    }
+
+    void MovementFunction() { 
+    if (Input.GetKey(KeyCode.A))
+        {
+            Doodler.transform.eulerAngles = new Vector2(0, 180);
+            Doodler.transform.Translate(Vector2.right * 5f * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Doodler.transform.eulerAngles = new Vector2(0, 0);
+            Doodler.transform.Translate(Vector2.right * 5f * Time.deltaTime);
+
+        }
+    }
+   
+}
---
+++ Mr.President/Assets/Move.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+
+{
+    private Rigidbody2D rb;
+    private float movespeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movespeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+
+            Touch touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (touch.position.x < Screen.width / 2)
+                    {
+                        rb.transform.eulerAngles = new Vector2(0, 180);
+                        rb.velocity = new Vector2(-movespeed, 0f);
+
+                    }
+
+                    if (touch.position.x > Screen.width / 2)
+                    {
+                        rb.transform.eulerAngles = new Vector2(0, 0);
+                        rb.velocity = new Vector2(movespeed, 0f);
+                    }
+                    break;
+
+                case TouchPhase.Ended:
+                    rb.velocity = new Vector2(0f, 0f);
+                    break;
+            }
+
+        }
+    }
+}
